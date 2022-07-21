bad7db1042c422630f78de627ac8215365a2d90f
Pawel <pawel.gierczak97@gmail.com>
2022-07-21 16:54:20 +0200

    Added back to top button and some changes in Entry and About

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,14 @@ import { GiSkills } from "react-icons/gi";
 import { MdWeb , MdOutlineContactless } from "react-icons/md";
 import { AiFillRightCircle } from "react-icons/ai";
 import { AiFillLeftCircle } from "react-icons/ai";
+import { TbArrowTopCircle } from "react-icons/tb";
 import { useState } from 'react';
 
 
 const Header = () => {
     
     const [toggle, setToggle] = useState(true);
-
+    const [back, setBack] = useState(true);
     const toggleMenu = event =>  {
         setToggle (!toggle);
     };
@@ -19,14 +20,37 @@ const Header = () => {
     } else {
       moreButton = <AiFillRightCircle onClick={toggleMenu} />;
     }
+    var backButton; 
+    const handleScroll = event => {
+        let scrollTop = window.scrollY;
+        if(scrollTop > 50){
+            setBack(true);
+            
+        }
+        else{
+            setBack(false);
+           
+        }
+        
+    }
+    if(back){
+        backButton = <div className="back_to_top"><a href="#"><TbArrowTopCircle/></a></div>;
+    }
+    else{
+        backButton = null;
+    }
     
+    
+    window.addEventListener('scroll', handleScroll);
+
+   
 
     return (
         <div id="header">
             <div className={"header_nav" + (toggle ? '' : ' thin')}>
                 <div className="header_nav_link">
                     <b></b>
-                    <a href="#">
+                    <a href="#about">
                         <div className="header_nav_link__icon">
                             <SiSuperuser/>
                         </div>
@@ -85,6 +109,9 @@ const Header = () => {
 
                 
             </div>
+            {backButton}
+            
+            
         </div>
     )
 }
--- src/components/main_elements/entry.js
+++ src/components/main_elements/entry.js
@@ -8,7 +8,15 @@ const Entry = () => {
 
     return (
         <div id="entry">
-           
+           <div className="entry_content">
+                <div className="entry_content_left">
+                    <h1>Paweł Gierczak</h1>
+                    <h2>Front-End Developer</h2>
+                </div>
+                <div className="entry_content_right">
+
+                </div>
+           </div>
         </div>
         
     )
